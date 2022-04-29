c960890b8d39503dcc390ddcb9fd626c912af7f0
Limebee <67772092+Limebee@users.noreply.github.com>
2022-04-28 21:05:07 -0400

    Make a function that will return a statement

---
+++ Coding Challenges/2022-04-28.js
@@ -0,0 +1,13 @@
+//Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+
+
+
+
+
+function greet(name){
+    return `Hello, ${name} how are you doing today?`
+  }
+  
+//created a function
+  //used a template literal to make whatever input appear in the string
+  //Returned the template literal
