ad83e8f443aaafc52396fbb71902e577c5535925
Limebee <67772092+Limebee@users.noreply.github.com>
2022-05-07 23:43:23 -0400

    Function Greet

---
+++ Coding Challenges/2022-05-07.js
@@ -0,0 +1,10 @@
+//Make a simple function called greet that returns the most-famous "hello world!".
+
+
+function greet(){
+    return "hello world!"
+  }
+
+
+  //created function greet
+  //made the function return hello world
