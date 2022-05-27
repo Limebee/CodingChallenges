48e3708e128127e86714e08900bd1753a607911f
Karen Jean Louis <67772092+Limebee@users.noreply.github.com>
2022-05-26 23:34:41 -0400

    Difference of Volumes of Cuboids

---
+++ Coding Challenges/2022-05-26.js
@@ -0,0 +1,20 @@
+//In this simple exercise, you will create a program that will take two lists of integers, a and b. Each list will consist of 3 positive integers above 0, 
+//representing the dimensions of cuboids a and b. You must find the difference of the cuboids' 
+//volumes regardless of which is bigger.
+
+//For example, if the parameters passed are ([2, 2, 3], [5, 4, 1]), the volume of a is 12 and the volume of b is 20. Therefore, the function should return 8.
+
+
+
+
+
+function findDifference(a, b) {
+    let aVolume = a.reduce((current,total) => current * total, 1) 
+    let bVolume = b.reduce((current,total) => current * total, 1)
+    return Math.abs(aVolume - bVolume)
+}
+
+
+//for each input use reduce to mutiply all of the numbers together
+//subtract each result
+//get absolute value of the result
