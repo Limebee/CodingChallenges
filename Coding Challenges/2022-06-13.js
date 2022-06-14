f5bf09b21bfd7782dd93e9f9883e30a0c5df50b9
Karen Jean Louis <67772092+Limebee@users.noreply.github.com>
2022-06-13 23:33:21 -0400

    Enumerable Magic #25 - Take the First N Elements

---
+++ Coding Challenges/2022-06-13.js
@@ -0,0 +1,17 @@
+/*
+Create a function that accepts a list/array and a number n, and returns a list/array of the first n elements from the list/array.
+
+
+*/
+
+
+
+
+function take(arr, n) {
+    return arr.slice(0, n)
+  }
+
+
+  //use slice
+  //start at index 0 an stop at n
+  //return new array
